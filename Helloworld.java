5dd3b2f3a4122c626ba8192f07428e567e7da897
your name <your_email@example.com>
2023-07-12 09:06:34 +0800

    day 2 exercise

---
+++ Helloworld.java
@@ -0,0 +1,8 @@
+public class Helloworld {
+  public static void main(String[] args) {
+    // print out something
+    System.out.println("I'm Vincent"); // end with semi-colon
+    System.out.println("Hello"); // end with semi-colon
+    System.out.println("hello   *#&$%(@#%) 中心"); // end with semi-colon
+  }
+}
